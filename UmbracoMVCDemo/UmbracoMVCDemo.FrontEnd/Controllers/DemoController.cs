3c0daabd0e7f7987d14c40c98748a528b3d0df5d
Bart De Meyer <bart@bartdemeyer.be>
2013-01-04 13:04:59 +0100

    Demo of Using MVC frontend pages in Umbraco 4.11.1  on blog.bartdemeyer.be
    Demo project of Using MVC frontend pages in Umbraco 4.11.1 post on
    blog.bartdemeyer.be

---
+++ UmbracoMVCDemo/UmbracoMVCDemo.FrontEnd/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+
+namespace UmbracoMVCDemo.FrontEnd.Controllers
+{
+    public class DemoController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        //
+        // GET: /Demo/
+
+        public override ActionResult Index(RenderModel model)
+        {
+            //Do some stuff here, then return the base method
+            return base.Index(model);
+        }
+
+        public ActionResult Demo(RenderModel model)
+        {
+            return View(model);
+        }
+
+    }
+}
---
+++ UmbracoMVCDemo/UmbracoMVCDemo.FrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UmbracoMVCDemo.FrontEnd.MvcApplication" Language="C#" %>
